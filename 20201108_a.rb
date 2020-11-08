3cba57cee7745b816c7d8a744247e153972a9404
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-11-08 22:53:54 +0900

    Add files via upload

---
+++ 20201108_a.rb
@@ -0,0 +1,15 @@
+fruits = {"apple": "100", "orange": "200"}
+
+p fruits.hash
+p fruits[:apple]
+p Math::PI
+sales = [1,2,3,4,5]
+p sales[0...2]
+sales += [9,5,6]
+p sales
+p sales.sort
+p sales.size
+
+sales -= [9]
+
+p sales

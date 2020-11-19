5dbbe6d651cfd29a98a0c98e7a000d7302e86f36
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-11-19 21:27:38 +0900

    Create element.rb

---
+++ 20201120.rb/element.rb
@@ -0,0 +1,13 @@
+[1, 2, 3, 4, 5].each do |n|
+  p n
+end
+
+{height: 170, weight: 50}.each do |key, value|
+  p key
+  p value
+end
+
+{height: 170, weight: 50}.each do |element|
+  p element
+end
+

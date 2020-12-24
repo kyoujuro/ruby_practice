369bc9a63f5ab118104a4db826757712b2a78e91
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-12-24 22:21:36 +0900

    Update practice.rb

--- 20201224/practice.rb
+++ 20201224/practice.rb
@@ -1,2 +1,16 @@
 TEST = ["first", "second"]
 TEST.freeze
+
+"12345".split(/3/)
+# ["12", "45"]
+
+dirs = ['usr', 'Desktop', 'practice']
+#=> ["usr", "Desktop", "practice"]
+File.join *dirs
+#=> "usr/Desktop/practice"
+File.join(dirs)
+#=> "usr/Desktop/practice"
+File.join(*dirs)
+#=> "usr/Desktop/practice"
+dirs.map &:length
+#=> [3, 7, 8]

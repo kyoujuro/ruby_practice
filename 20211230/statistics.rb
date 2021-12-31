646fe399b25e9df827b64bbf35ee49d0bfbce6b2
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2021-12-31 16:50:29 +0900

    add

--- 20211230/statistics.rb
+++ 20211230/statistics.rb
@@ -12,18 +12,27 @@ module FundamentalStatistics
         def stdvar
             Math.sqrt(var)
         end
+
+        def convar
+            array_x = map{|x| x[0]}
+            array_y = map{|y| y[0]}
+            @avg_x = array_x.avg
+            @avg_y = array_y.avg
+            inject(0) { |res, n| res + (n[0] - @avg_x) * (n[1] - @avg_y)}.fdiv(size)
+        end 
     end
 end
 
 class Summary
     using FundamentalStatistics
-    def summary
-        x = [1, 2, 3, 4, 5]
+    def summary(x, y)
         x_avg = x.avg
         x_var = x.var
         x_stdvar = x.stdvar
-        p x_avg, x_var, x_stdvar
+        array = x.zip(y)
+        convar = array.convar
+        p x_avg, x_var, x_stdvar, convar
     end
 end
 
-Summary.new.summary
+Summary.new.summary([1, 2, 3 ,4, 5],[6, 7, 8, 9, 10])
